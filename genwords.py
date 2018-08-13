4e5c93298602faaf27b9f225b3fc78783e921b9c
Lincoln Quirk <lincolnq@gmail.com>
2018-08-13 01:21:02 +0000

    improved styles and upside-down text

--- genwords.py
+++ genwords.py
@@ -12,38 +12,39 @@ font-family: sans-serif;
 font-size: 24pt;
 }}
 
-ul {{
-list-style-type: none;
+td {{ padding: 0.5em 1em; border: 1px solid #eee; text-align: center; }}
+table {{ border-collapse: collapse; }}
+.rev {{
+color: #aaa;
+transform: rotate(180deg);
 }}
+p {{ margin: 0; }}
 
-li {{
-display: inline-block;
-width: 19%;
-text-align: center;
-padding: 1em 0;
-}}
 </style>
 <body>
-<ul>
+<table>
 {items}
-</ul>
+</table>
 </body>
 </html>
 '''
 
-item_template = '''
-<li>{item}</li>
-'''
+def render(items):
+    def mkrow(row):
+        """Given 5 words in a row, render the row"""
+        return ''.join('<td><p class="fwd">{word}</p><p class="rev">{word}</p></td>'.format(word=word) for word in row)
+    def splitrows(items):
+        """Given 25 items, split it into a list of 5-item rows"""
+        return [items[i:i+5] for i in range(0, 25, 5)]
+
+    items_html = '\n'.join('<tr>{items}</tr>'.format(items=mkrow(row)) for row in splitrows(items))
+    html = template.format(items=''.join(items_html))
+    return html
 
 def generate():
     selection = random.sample(words, 25)
+    html = render(selection)
 
-    items = []
-    for w in selection:
-        item_html = item_template.format(item=w)
-        items.append(item_html)
-
-    html = template.format(items=''.join(items))
     outf.write(html)
 
 generate()
